1bed18008f9227cd43f7253d37804f309ec2c143
AndriiMishch <anderito02@gmail.com>
2023-10-06 11:03:55 +0200

    Update pixabayAPI.js

--- src/services/pixabayAPI.js
+++ src/services/pixabayAPI.js
@@ -1,20 +1,10 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '38650102-d85801c337edddd96f74bbd65';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-const imagesPerPage = 12;
-
-export async function getImagesBySearchQuery(query, currentPage) {
-  try {
-    const response = await axios.get(
-      `${BASE_URL}?key=${API_KEY}&q=${query}&page=${currentPage}&per_page=${imagesPerPage}&image_type=photo&orientation=horizontal`
-    );
-
-    if (currentPage <= Math.ceil(response.data.totalHits / imagesPerPage)) {
-      return response.data;
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
+export const getPixabayPictures = (searchQuery, page, perPage) => {
+  return axios.get(
+    `?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`
+  );
+};
