3d63dcf373708ce9c78e1f973c6201fb3adc1694
AndriiMishch <anderito02@gmail.com>
2023-10-06 11:03:08 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,59 +1,25 @@
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { Component } from 'react';
-
-import { GlobalStyle } from 'GlobalStyle.styled';
-import { AppContainer } from './App.styled';
-
-import Searchbar from '../Searchbar';
+import React, { Component } from 'react';
+import SearchBar from '../Searchbar';
+import css from './App.module.css';
 import ImageGallery from '../ImageGallery';
-import Modal from 'components/Modal';
 
 export default class App extends Component {
   state = {
-    searchValue: '',
-    modalOpened: false,
-    modalImage: null,
+    searchQuery: '',
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.searchValue !== this.state.searchValue) {
-      localStorage.setItem(
-        'search-value',
-        JSON.stringify(this.state.searchValue)
-      );
-    }
-  }
-
-  onSearchFormSubmit = searchValue => {
-    this.setState({ searchValue });
-  };
-
-  toggleModal = img => {
-    this.setState(prevState => ({
-      modalOpened: !prevState.modalOpened,
-      modalImage: img,
-    }));
+  handleSubmit = value => {
+    this.setState({ searchQuery: value });
   };
 
   render() {
-    const { searchValue, modalOpened, modalImage } = this.state;
+    const { searchQuery } = this.state;
 
     return (
-      <>
-        <GlobalStyle $modalOpened={modalOpened} />
-        <AppContainer>
-          <Searchbar onSubmit={this.onSearchFormSubmit} />
-          <ImageGallery
-            searchValue={searchValue}
-            toggleModal={this.toggleModal}
-          />
-          {modalOpened && (
-            <Modal toggleModal={this.toggleModal} img={modalImage} />
-          )}
-          <ToastContainer />
-        </AppContainer>
-      </>
+      <div className={css.App}>
+        <SearchBar onSubmit={this.handleSubmit} />
+        <ImageGallery searchQuery={searchQuery} />
+      </div>
     );
   }
 }
